455ca8dd6948b8d2bbc18efd6cb12019d17e5066
Dimanteo <dikolobay@gmail.com>
2021-04-07 22:01:59 +0300

    Added Core methods definitions

--- LLVM/Core.cpp
+++ LLVM/Core.cpp
@@ -1,24 +1,22 @@
 #include "Core.hpp"
 #include "Instruction.hpp"
 
-Core::Core() : m_memory(new word_t [RAM_SIZE]), m_running(false) {};
+Core::Core()
+    : m_memory(new char[RAM_SIZE * sizeof(word_t)]), m_running(false){};
 
-Core::~Core() {
-    delete [] (m_memory);
-}
+Core::~Core() { delete[](m_memory); }
 
 void Core::run(char *code, size_t codeOffset, size_t codeSz) {
     char *entry = code + codeOffset;
-
     llvm::LLVMContext context;
     llvm::Module module("top", context);
     llvm::IRBuilder<> builder(context);
-
-    llvm::FunctionType *funcType = 
+    llvm::FunctionType *funcType =
         llvm::FunctionType::get(builder.getVoidTy(), false);
-    llvm::Function *mainFunc = 
-        llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
-    llvm::BasicBlock *startBB = llvm::BasicBlock::Create(context, "start", mainFunc);
+    llvm::Function *mainFunc = llvm::Function::Create(
+        funcType, llvm::Function::ExternalLinkage, "main", module);
+    llvm::BasicBlock *startBB =
+        llvm::BasicBlock::Create(context, "start", mainFunc);
 
     bblockCache.insert({codeOffset, startBB});
 
@@ -34,13 +32,15 @@ void Core::run(char *code, size_t codeOffset, size_t codeSz) {
             // Creating new basic block for jump destination
             size_t dest = insn.getArg(0);
             if (bblockCache.find(dest) != bblockCache.end()) {
-                llvm::BasicBlock *destBB = llvm::BasicBlock::Create(context, std::to_string(dest), mainFunc);
+                llvm::BasicBlock *destBB = llvm::BasicBlock::Create(
+                    context, std::to_string(dest), mainFunc);
                 bblockCache.insert({dest, destBB});
             }
             // End current basic block and switch to new one.
             size_t offset = nextPC - code;
             if (bblockCache.find(offset) != bblockCache.end()) {
-                llvm::BasicBlock *nextBB = llvm::BasicBlock::Create(context, std::to_string(offset), mainFunc);
+                llvm::BasicBlock *nextBB = llvm::BasicBlock::Create(
+                    context, std::to_string(offset), mainFunc);
                 bblockCache.insert({offset, nextBB});
             }
         }
@@ -53,25 +53,52 @@ void Core::run(char *code, size_t codeOffset, size_t codeSz) {
         pc_insn.second.generateIR(&builder, *this);
     }
 
+    // Printing for debug
     std::cout << "## [LLVM IR] DUMP ##\n";
-
     std::string ir_str;
     llvm::raw_string_ostream ir_os(ir_str);
     module.print(ir_os, nullptr);
     ir_os.flush();
     std::cout << ir_str;
-
-    std::cout << "\n## [LLVM EE] RUN ##\n";
-
     llvm::InitializeNativeTarget();
     llvm::InitializeNativeTargetAsmPrinter();
 
-    std::cout << "## [LLVM EE] END ##\n";
-
-    llvm::ExecutionEngine *ee = llvm::EngineBuilder(std::unique_ptr<llvm::Module>(&module)).create();
+    // Execute IR
+    llvm::ExecutionEngine *ee =
+        llvm::EngineBuilder(std::unique_ptr<llvm::Module>(&module)).create();
     ee->InstallLazyFunctionCreator(lazyFunctionCreator);
     ee->finalizeObject();
     std::vector<llvm::GenericValue> noargs;
     m_running = true;
     ee->runFunction(mainFunc, noargs);
 }
+
+void Core::push(word_t val) { m_stack.push_back(val); }
+
+word_t Core::pop() {
+    word_t val = m_stack.back();
+    m_stack.pop_back();
+    return val;
+}
+
+reg_t Core::getReg(REG_CODE regi) const { return regFile[regi]; }
+
+void Core::setReg(REG_CODE regi, reg_t val) { regFile[regi] = val; }
+
+void Core::read(address_t adr, size_t nbytes, char *dest) const {
+    memcpy(dest, m_memory + adr, nbytes);
+}
+
+void Core::readWord(address_t adr, word_t *dest) const {
+    *dest = reinterpret_cast<word_t *>(m_memory)[adr];
+}
+
+void Core::write(address_t adr, size_t nbytes, char *src) {
+    memcpy(m_memory + adr, src, nbytes);
+}
+
+void Core::writeWord(address_t adr, word_t src) const {
+    reinterpret_cast<word_t *>(m_memory)[adr] = src;
+}
+
+void Core::stop() { m_running = false; }
--- LLVM/Core.hpp
+++ LLVM/Core.hpp
@@ -1,14 +1,14 @@
 #pragma once
-#include <vector>
-#include <unordered_map>
 #include <iostream>
-#include <llvm-10/llvm/IR/LLVMContext.h>
-#include <llvm-10/llvm/IR/Module.h>
-#include <llvm-10/llvm/IR/IRBuilder.h>
-#include <llvm-10/llvm/IR/BasicBlock.h>
 #include <llvm-10/llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm-10/llvm/ExecutionEngine/GenericValue.h>
+#include <llvm-10/llvm/IR/BasicBlock.h>
+#include <llvm-10/llvm/IR/IRBuilder.h>
+#include <llvm-10/llvm/IR/LLVMContext.h>
+#include <llvm-10/llvm/IR/Module.h>
 #include <llvm-10/llvm/Support/TargetSelect.h>
+#include <unordered_map>
+#include <vector>
 
 #include "../linker.h"
 
@@ -16,24 +16,25 @@ using word_t = int;
 using reg_t = int;
 using address_t = size_t;
 
-class Core
-{
-        std::vector<word_t> m_stack;
-        reg_t regFile[NREGS];
-        size_t m_pc;
-        word_t *m_memory;
-        bool m_running;
-        std::unordered_map<size_t, llvm::BasicBlock*> bblockCache;
+class Core {
+    std::vector<word_t> m_stack;
+    reg_t regFile[NREGS];
+    size_t m_pc;
+    char *m_memory;
+    bool m_running;
+    std::unordered_map<size_t, llvm::BasicBlock *> bblockCache;
 
-    public:
-        Core();
-        ~Core();
-        void run(char *code, size_t codeOffset, size_t codeSz);
-        void push(word_t val);
-        word_t pop();
-        reg_t getReg(REG_CODE regi) const;
-        void setReg(REG_CODE regi);
-        void read(address_t adr) const;
-        void write(address_t adr);
-        void stop();
+  public:
+    Core();
+    ~Core();
+    void run(char *code, size_t codeOffset, size_t codeSz);
+    void push(word_t val);
+    word_t pop();
+    reg_t getReg(REG_CODE regi) const;
+    void setReg(REG_CODE regi, reg_t val);
+    void read(address_t adr, size_t nbytes, char *dest) const;
+    void readWord(address_t adr, word_t *dest) const;
+    void write(address_t adr, size_t nbytes, char *src);
+    void writeWord(address_t adr, word_t src) const;
+    void stop();
 };
